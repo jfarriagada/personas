4fe5e280efb9722ee8386b6662c46197f7de13cc
farriagada <jfarriagada91@gmail>
2020-05-09 12:43:02 -0300

    se conecto el componente home con redux

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { getUsers } from '../actions'
 
 class Home extends Component {
+
+    componentWillMount(){
+        this.props.getUsers()
+    }
+
     render(){
         return(
             <div>home</div>
@@ -9,4 +16,20 @@ class Home extends Component {
     }
 }
 
-export default Home
+function mapStateToProps(state){
+    return {
+        users: state.getUsers
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    /**
+     * bindActionCreators se utiliza para que las funciones de los
+     * actions(getUsers) se puedan utulizar dentro del componente
+     */
+    return bindActionCreators({
+        getUsers
+    }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
