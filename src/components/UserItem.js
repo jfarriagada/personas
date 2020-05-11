824526478ce9660d6451a67e1a03e00dfda5e998
farriagada <jfarriagada91@gmail>
2020-05-11 16:38:36 -0400

    se agrego un spinner a la carga de usuarios

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,18 +1,52 @@
-import React, { Component } from 'react'
+import React from 'react';
 import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-class UserItem extends Component {
-    render(){
-        const { id, name, last_name, facebook } = this.props
-        return(
-            <div>
-                <p>{ name }</p>
-                <p>{ last_name }</p>
-                <p>{ facebook }</p>
-            </div>
-        )
-    }
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function UserItem(props) {
+  const classes = useStyles()
+  const bull = <span className={classes.bullet}>•</span>
+
+  const { id, name, last_name, facebook } = props
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          { name}
+        </Typography>
+        <Typography variant="h5" component="h2">
+            { last_name }{bull}{ facebook }
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 }
+
 
 UserItem.protoTypes = {
     name: PropTypes.string.isRequired,
@@ -20,5 +54,3 @@ UserItem.protoTypes = {
     id: PropTypes.number.isRequired,
     facebook: PropTypes.string
 }
-
-export default UserItem
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { getUsers } from '../actions'
-import 'materialize-css/dist/css/materialize.min.css'
 import UserItem from '../components/UserItem'
+import Grid from '@material-ui/core/Grid'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
 
 class Home extends Component {
     constructor(){
@@ -14,9 +16,11 @@ class Home extends Component {
         this.props.getUsers()
     }
 
+    
     render(){
         console.log("render props", this.props);
         let users = []
+
         if(this.props.users.data){
             users = this.props.users.data.map((item, index, array) => {
                 return (
@@ -29,6 +33,19 @@ class Home extends Component {
                     />                
                 )
             })
+        }
+
+        if(this.props.users.type === "START_GET_USERS"){
+            return (
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+            >
+                <CircularProgress color="primary" />
+            </Grid>
+            )
         }
 
         return(
